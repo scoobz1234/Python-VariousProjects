90f388574721f7db2ace22139d9aadba95c8a09d
scoobz1234 <scoobz1234@gmail.com>
2017-05-22 23:54:20 -0700

    gitHandler

--- Git_Light.py
+++ Git_Light.py
@@ -1,107 +1,94 @@
-"""GUI GIT Program"""
+"""       Lightweight GIT Program    """
 
-#   ***** IMPORTS *****
+"""       ***** IMPORTS *****        """
 from subprocess import call, Popen, PIPE
 import os
-
-#  ***** FUNCTIONS *****
-def exit_prompt():
-    print ("Thanks for using Git Handler")
-    quit()
-
+from time import sleep
+"""       ***** FUNCTIONS *****      """
 def git_push():
 # solicit input
-    print("Whats your Username?")
-    user_name = input(">>> ")
-    print("Whats your Repo's Name?")
-    repo_name = input(">>> ")
+    user_name = input("What's your UserName?\n>>> ")
+    repo_name = input("What's your Repo's Name?\n>>> ")
 # do stuff with the data
     call('git push https://' + user_name + '@github.com/' + user_name + '/' + repo_name + '.git',shell=True)
-    print("********************************")
-    print("********PUSH COMPLETED********")
-    print("********************************")
+    print("\n********************************\n********PUSH COMPLETED********\n********************************\n")
 
 def git_clone():
 # solicit input
-    print("Whats your Username?")
-    user_name = input(">>> ")
-    print("Whats your Repo's Name?")
-    repo_name = input(">>> ")
+    user_name = input("What's the Username?\n>>>  ")
+    repo_name = input("What's the Repo's Name?\n>>> ")
 # do stuff with the data
     call('git clone https://' + user_name + '@github.com/' + user_name + '/' + repo_name + '.git',shell=True)
-    print("********************************")
-    print("********CLONE COMPLETED********")
-    print("********************************")
+    print("\n********************************\n********CLONE COMPLETED********\n********************************\n")
 
 def git_status():
     call("git status",shell=True)
 
 def git_commit():
     os.system('git commit -m"gitHandler"')
-    print("********************************")
-    print("********COMMIT COMPLETED********")
-    print("********************************\n")
+    print("\n********************************\n********COMMIT COMPLETED********\n********************************\n")
 
 def git_add():
     call("git add -A",shell=True)
     git_commit()
 
-#   ***** SETUP *****
+"""        ***** INPUT *****     """
 
-print("Welcome to Stephen's Git Handler!")
-print("type Help at any time to get a list of commands!\n")
+print("********************************\nWelcome to the Auto Git Handler!\n********************************\n\n")
+sleep(1)
+print("*********************************\nCreated by Stephen Ouellette 17'!\n*********************************\n\n")
+sleep(3)
+print("type [Help] or [H] at any time to get a list of commands!\n")
 while True:
-    print("you have access to the following commands:\n\n[Clone]","[Push]","[Status]","[Add_Commit]","[Exit]\n\nalternatly try [C][P][S][A][E] respectivly\n")
-    # print("[Clone]","[Push]","[Status]","[Commit]","[Exit]\n")
-    # print("***NOTE***","Commit command will perform Add and Commit\n")
-    user_command = str(input(">>> "))
-    user_command = user_command.upper()
+    print("you have access to the following commands:\n\n[Clone]","[Push]","[Status]","[Add_Commit]","[Exit]\n\nalternatly try [C] [P] [S] [A] [E] respectivly\n")
+    user_command = str(input(">>> ")).upper()
 
     if user_command == "CLONE" or user_command == "C":
-        print("You have selected the Clone function, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
-        if confirm == "Y" or "YES":
+        confirm = str(input("You have selected the Clone function, are you sure?\n(Y/N)\n>>> ")).upper()
+        if confirm == "Y" or confirm == "YES":
             git_clone()
         else:
             continue
 
     elif user_command == "STATUS" or user_command == "S":
-        print("You have selected the Status command, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
-        if confirm == "Y" or "YES":
+        confirm = str(input("You have selected the Status command, are you sure?\n(Y/N)\n>>> ")).upper()
+        if confirm == "Y" or confirm == "YES":
             git_status()
             continue
         else:
             continue
 
     elif user_command == "PUSH" or user_command == "P":
-        print("You have selected the Push command, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
-        if confirm == "Y" or "YES":
+        confirm = str(input("You have selected the Push command, are you sure?\n(Y/N)\n>>> ")).upper()
+        if confirm == "Y" or confirm == "YES":
             git_push()
             continue
         else:
             continue
 
     elif user_command == "ADD_COMMIT" or user_command == "A" or user_command == "COMMIT":
-        print("You have selected the Commit command, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
-        if confirm == "Y" or "YES":
+        confirm = str(input("You have selected the Add/Commit command, are you sure?\n(Y/N)\n>>> ")).upper()
+        if confirm == "Y" or confirm == "YES":
             git_add()
             continue
         else:
             continue
 
+    elif user_command == "HELP" or user_command == "H":
+        while True:
+            print("\nType [A] or [ADD_Commit]-------- runs the git add -A and git commit -m commands")
+            print("Type [C] or [Clone]------------- runs the git clone command")
+            print("Type [E] or [Exit]-------------- runs the exit command (leaves the program)")
+            print("Type [H] or [Help]-------------- runs the help command and lists these commands")
+            print("Type [P] or [Push]-------------- runs the git push command in terminal")
+            print("Type [S] or [Status]------------ runs the git status command in terminal\n")
+            press_enter_to_continue = input("Press [Enter] to Continue...")
+            print("")
+            break
+
     elif user_command == "EXIT" or user_command == "E":
-        print("You have selected the Exit command, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
-        if confirm == "Y" or "YES":
-            # exit_prompt()
+        confirm = str(input("You have selected the Exit command, are you sure?\n(Y/N)\n>>> ")).upper()
+        if confirm == "Y" or confirm == "YES":
             break
         else:
             continue
--- Projects/Git_Light.py
+++ Projects/Git_Light.py
@@ -1,64 +1,56 @@
-"""GUI GIT Program"""
+                            """        Lightweight GIT Program          """
 
-#   ***** IMPORTS *****
+                            """       ***** IMPORTS *****        """
 from subprocess import call, Popen, PIPE
 import os
-
-#  ***** FUNCTIONS *****
+from time import sleep
+                            """       ***** FUNCTIONS *****      """
 def git_push():
 # solicit input
-    print("Whats your Username?")
-    user_name = input(">>> ")
-    print("Whats your Repo's Name?")
-    repo_name = input(">>> ")
+    user_name = input("What's your UserName?\n>>> ")
+    repo_name = input("What's your Repo's Name?\n>>> ")
 # do stuff with the data
     call('git push https://' + user_name + '@github.com/' + user_name + '/' + repo_name + '.git',shell=True)
-    print("********************************")
-    print(" ********PUSH COMPLETED********")
-    print("********************************")
+    print("\n********************************\n********PUSH COMPLETED********\n********************************\n")
 
 def git_clone():
 # solicit input
-    # print("Whats the Username?")
     user_name = input("What's the Username?\n>>>  ")
-    # print("Whats the Repo's Name?")
     repo_name = input("What's the Repo's Name?\n>>> ")
 # do stuff with the data
     call('git clone https://' + user_name + '@github.com/' + user_name + '/' + repo_name + '.git',shell=True)
-    print("********************************\n********CLONE COMPLETED********\n********************************\n")
+    print("\n********************************\n********CLONE COMPLETED********\n********************************\n")
 
 def git_status():
     call("git status",shell=True)
 
 def git_commit():
     os.system('git commit -m"gitHandler"')
-    print("********************************\n********COMMIT COMPLETED********\n********************************\n")
-    # print(" ********COMMIT COMPLETED******")
-    # print("********************************\n")
+    print("\n********************************\n********COMMIT COMPLETED********\n********************************\n")
 
 def git_add():
     call("git add -A",shell=True)
     git_commit()
 
-#   ***** SETUP *****
+                            """        ***** INPUT *****     """
 
-print("Welcome to Stephen's Git Handler!")
+print("*****************************\nWelcome to the Auto Git Handler!\n*****************************\n\n")
+print("******************************\nCreated by Stephen Ouellette 17'!\n******************************\n\n")
+sleep(3)
 print("type Help at any time to get a list of commands!\n")
 while True:
     print("you have access to the following commands:\n\n[Clone]","[Push]","[Status]","[Add_Commit]","[Exit]\n\nalternatly try [C][P][S][A][E] respectivly\n")
     user_command = str(input(">>> ")).upper()
 
     if user_command == "CLONE" or user_command == "C":
-        print("You have selected the Clone function, are you sure?")
-        confirm = str(input("(Y/N)\n>>> ")).upper()
+        confirm = str(input("You have selected the Clone function, are you sure?\n(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             git_clone()
         else:
             continue
 
     elif user_command == "STATUS" or user_command == "S":
-        print("You have selected the Status command, are you sure?")
-        confirm = str(input("(Y/N)\n>>> ")).upper()
+        confirm = str(input("You have selected the Status command, are you sure?\n(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             git_status()
             continue
@@ -66,8 +58,7 @@ while True:
             continue
 
     elif user_command == "PUSH" or user_command == "P":
-        print("You have selected the Push command, are you sure?")
-        confirm = str(input("(Y/N)\n>>> ")).upper()
+        confirm = str(input("You have selected the Push command, are you sure?\n(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             git_push()
             continue
@@ -75,8 +66,7 @@ while True:
             continue
 
     elif user_command == "ADD_COMMIT" or user_command == "A" or user_command == "COMMIT":
-        print("You have selected the Commit command, are you sure?")
-        confirm = str(input("(Y/N)\n>>> ")).upper()
+        confirm = str(input("You have selected the Add/Commit command, are you sure?\n(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             git_add()
             continue
@@ -84,8 +74,7 @@ while True:
             continue
 
     elif user_command == "EXIT" or user_command == "E":
-        print("You have selected the Exit command, are you sure?")
-        confirm = str(input("(Y/N)\n>>> ")).upper()
+        confirm = str(input("You have selected the Exit command, are you sure?\n(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             break
         else:
