d7fa10397d5914cb8f8b2256227b1e3d20a2bae5
scoobz1234 <scoobz1234@gmail.com>
2017-05-22 23:17:17 -0700

    gitHandler

--- Projects/Git_Light.py
+++ Projects/Git_Light.py
@@ -5,10 +5,6 @@ from subprocess import call, Popen, PIPE
 import os
 
 #  ***** FUNCTIONS *****
-def exit_prompt():
-    print ("Thanks for using Git Handler")
-    quit()
-
 def git_push():
 # solicit input
     print("Whats your Username?")
@@ -18,29 +14,27 @@ def git_push():
 # do stuff with the data
     call('git push https://' + user_name + '@github.com/' + user_name + '/' + repo_name + '.git',shell=True)
     print("********************************")
-    print("********PUSH COMPLETED********")
+    print(" ********PUSH COMPLETED********")
     print("********************************")
 
 def git_clone():
 # solicit input
-    print("Whats your Username?")
-    user_name = input(">>> ")
-    print("Whats your Repo's Name?")
-    repo_name = input(">>> ")
+    # print("Whats the Username?")
+    user_name = input("What's the Username?\n>>>  ")
+    # print("Whats the Repo's Name?")
+    repo_name = input("What's the Repo's Name?\n>>> ")
 # do stuff with the data
     call('git clone https://' + user_name + '@github.com/' + user_name + '/' + repo_name + '.git',shell=True)
-    print("********************************")
-    print("********CLONE COMPLETED********")
-    print("********************************")
+    print("********************************\n********CLONE COMPLETED********\n********************************\n")
 
 def git_status():
     call("git status",shell=True)
 
 def git_commit():
     os.system('git commit -m"gitHandler"')
-    print("********************************")
-    print("********COMMIT COMPLETED********")
-    print("********************************\n")
+    print("********************************\n********COMMIT COMPLETED********\n********************************\n")
+    # print(" ********COMMIT COMPLETED******")
+    # print("********************************\n")
 
 def git_add():
     call("git add -A",shell=True)
@@ -52,15 +46,11 @@ print("Welcome to Stephen's Git Handler!")
 print("type Help at any time to get a list of commands!\n")
 while True:
     print("you have access to the following commands:\n\n[Clone]","[Push]","[Status]","[Add_Commit]","[Exit]\n\nalternatly try [C][P][S][A][E] respectivly\n")
-    # print("[Clone]","[Push]","[Status]","[Commit]","[Exit]\n")
-    # print("***NOTE***","Commit command will perform Add and Commit\n")
-    user_command = str(input(">>> "))
-    user_command = user_command.upper()
+    user_command = str(input(">>> ")).upper()
 
     if user_command == "CLONE" or user_command == "C":
         print("You have selected the Clone function, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
+        confirm = str(input("(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             git_clone()
         else:
@@ -68,8 +58,7 @@ while True:
 
     elif user_command == "STATUS" or user_command == "S":
         print("You have selected the Status command, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
+        confirm = str(input("(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             git_status()
             continue
@@ -78,8 +67,7 @@ while True:
 
     elif user_command == "PUSH" or user_command == "P":
         print("You have selected the Push command, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
+        confirm = str(input("(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             git_push()
             continue
@@ -88,8 +76,7 @@ while True:
 
     elif user_command == "ADD_COMMIT" or user_command == "A" or user_command == "COMMIT":
         print("You have selected the Commit command, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
+        confirm = str(input("(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
             git_add()
             continue
@@ -98,10 +85,8 @@ while True:
 
     elif user_command == "EXIT" or user_command == "E":
         print("You have selected the Exit command, are you sure?")
-        confirm = str(input("(Y/n)\n>>> "))
-        confirm = confirm.upper()
+        confirm = str(input("(Y/N)\n>>> ")).upper()
         if confirm == "Y" or "YES":
-            # exit_prompt()
             break
         else:
             continue
